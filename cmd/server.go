f9d8c9b3ba777b35f5eff26e0b87a4bcda3e2082
mAdkins <software@doorways.org>
2022-07-25 17:16:39 -0700

    Cleanup, use go:embed for HTML

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"math/rand"
 	"net/http"
@@ -11,6 +12,9 @@ import (
 const AppName = "Most Inefficient Prime Number Sieve Web Server"
 const PortNum = 8123
 
+//go:embed display.html
+var page string
+
 // Main program sets up web handlers and starts web server
 func main() {
 	fmt.Println("Starting " + AppName)
@@ -19,7 +23,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Main page is in a constant defined below
 		fmt.Println("PageHandler")
-		fmt.Fprintf(w, page)
+		_, _ = fmt.Fprintf(w, page)
 	})
 	http.HandleFunc("/sieve/", func(w http.ResponseWriter, r *http.Request) {
 		// Sieve runs as server push and uses multiple server-side threads
@@ -35,7 +39,9 @@ func main() {
 		// Generate infinite numbers in this handler thread to hold it open
 		generator(c)
 	})
-	http.ListenAndServe(":"+strconv.Itoa(PortNum), nil)
+	if err := http.ListenAndServe(":"+strconv.Itoa(PortNum), nil); err != nil {
+		fmt.Printf("Error running web server: %s", err)
+	}
 
 	fmt.Println("Finished " + AppName)
 }
@@ -45,7 +51,9 @@ func main() {
 func commander(w http.ResponseWriter, commands chan string) {
 	for {
 		cmd := <-commands
-		w.Write([]byte("data: " + cmd + "\n\n"))
+		if _, err := w.Write([]byte("data: " + cmd + "\n\n")); err != nil {
+			panic(err)
+		}
 
 		// Flush after every dump
 		if f, ok := w.(http.Flusher); ok {
@@ -61,24 +69,24 @@ func filter(commands chan string, c chan int, prime int) {
 	var o chan int
 
 	// Creating a new filter means a new display box
-	commands <- ("make " + strconv.Itoa(prime))
+	commands <- "make " + strconv.Itoa(prime)
 
 	for {
 		// get the number from the input channel
 		number := <-c
 		args := strconv.Itoa(prime) + " " + strconv.Itoa(number)
 		// show the number under generator in the prime box and wait a bit
-		commands <- ("generator " + args)
+		commands <- "generator " + args
 		time.Sleep(time.Duration(250+rand.Intn(250)) * time.Millisecond)
 
 		if number%prime == 0 {
 			// number is divisible by this prime, it 'fails' and gets destroyed
-			commands <- ("fail " + args)
+			commands <- "fail " + args
 		} else {
 			// number not divisible, 'success' means going on the chain
-			commands <- ("pass " + args)
+			commands <- "pass " + args
 			time.Sleep(time.Duration(250+rand.Intn(250)) * time.Millisecond)
-			commands <- ("generator " + strconv.Itoa(prime))
+			commands <- "generator " + strconv.Itoa(prime)
 
 			if o == nil {
 				// there is no output channel yet so we have a new prime,
@@ -99,7 +107,7 @@ func generator(commands chan string) {
 
 	for i := 2; ; i++ {
 		// show new number in generator box and wait a bit
-		commands <- ("gen " + strconv.Itoa(i))
+		commands <- "gen " + strconv.Itoa(i)
 		time.Sleep(time.Duration(250+rand.Intn(250)) * time.Millisecond)
 
 		if c == nil {
@@ -111,122 +119,6 @@ func generator(commands chan string) {
 			c <- i
 		}
 
-		commands <- ("gen")
+		commands <- "gen"
 	}
 }
-
-const page = `
-<html>
-<head>
-  <title>Most Inefficient Prime Number Sieve</title>
-
-  <style>
-    div#blackboard {
-      background-color: linen;
-    }
-    div.box {
-      display: inline-block;
-      border: 4px outset violet;
-      padding: 3px;
-      margin:  3px;
-      background-color: mauve;
-      width: 50px;
-      height: 50px;
-    }
-    div.box span {
-      display: block;
-      height: 21px;
-      width: 46px;
-      padding: 2px;
-    }
-
-    div.box span.prime {
-      font-weight: bold;
-      background-color: gray;
-      color: azure;
-    }
-
-    div.box span.current {
-      float: right;
-      text-align: right;
-      vertical-align: bottom;
-    }
-
-    div.box span.fail {
-      background-color: red;
-    }
-
-    div.box span.pass {
-      background-color: lime;
-    }
-  </style>
-
-  <script>
-    console.log("Starting JavaScript")
-
-    var source = new EventSource("/sieve/");
-
-    function makeFilter(id) {
-        var filter = document.createElement('div');
-
-        filter.id = "filter-" + id;
-        filter.className = 'box';
-
-        var span = document.createElement('span');
-
-        span.textContent = id;
-        span.className = 'prime';
-        filter.appendChild(span);
-
-        span = document.createElement('span');
-        span.className = 'current';
-        filter.appendChild(span);
-
-        return filter;
-    }
-
-    source.onmessage = function(event) {
-        console.log(source + ".onmessage('" + event.data + "')")
-
-        var data = event.data.split(" ");
-        var what = data.shift();
-
-        if (what == "gen") {
-            document.getElementById("generator")
-                    .getElementsByClassName("current")[0]
-                    .textContent = data.shift();
-        } else if (what == "make") {
-            document.getElementById("blackboard")
-                    .appendChild(makeFilter(data.shift()));
-        } else if (what == "generator" || what == "pass" || what == "fail") {
-            var id = data.shift();
-            var current = document.getElementById('filter-' + id)
-                                  .getElementsByClassName("current")[0];
-
-            if (what == 'generator') {
-                current.className = 'current';
-                current.textContent = data.shift();
-            } else if (what == 'pass') {
-                current.className = 'current pass';
-            } else if (what == 'fail') {
-                current.className = 'current fail';
-            }
-        } else {
-            console.log("*** Unknown *** " + what + " | " + data);
-        }
-    };
-
-    console.log("Finished JavaScript")
-  </script>
-</head>
-
-<body>
-  <div id="blackboard">
-    <div id="generator" class="box">
-      <span class="prime">Gen#</span>
-      <span class="current"></span>
-    </div>
-  </div>
-</body>
-</html>
-`
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module doorways.org/sieve
 
-go 1.12
+go 1.16
