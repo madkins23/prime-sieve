c67f1c73169b8b827091c33f63c24e7a5f8c6780
mAdkins <software@doorways.org>
2023-06-28 16:20:31 -0700

    Fix class contract on display% and add unit tests for same

--- racket/display.rkt
+++ racket/display.rkt
@@ -1,24 +1,30 @@
 #lang racket/base
 
-(require racket/class)
-(require racket/contract)
-(require racket/match)
+(require racket/class
+         racket/contract
+         racket/match)
 
-(provide display%)
-(provide display?)
-(provide dsp-logger)
+(provide display%
+         display?
+         dsp-logger)
+
+(module+ test
+  (require rackunit))
 
 (define-logger dsp)
 
-(define display%
+(define/contract display%
+  (class/c [command (->m string? void?)]
+           [ready! (->m void?)]
+           [wait-ready (->m void?)]
+           (override [wait-done (->m void?)])
+           (override [do-command (->m string? void?)]))
   (class object%
     (super-new)
 
     (define ready-semaphore (make-semaphore))
-    (class/c [wait-ready (->m void?)])
     (define/public-final (wait-ready)
       (semaphore-wait ready-semaphore))
-    (class/c [ready! (->m void?)])
     (define/public-final (ready!)
       (semaphore-post ready-semaphore))
 
@@ -33,12 +39,24 @@
              ['done
               (log-dsp-debug "display worker thread done")])))))
 
-    (class/c [command (->m string? void?)])
     (define/public-final (command command)
       (thread-send worker command))
 
-    (abstract do-command wait-done)
-    (class/c (override [do-command (->m string? void?)]))
-    (class/c (override [ wait-done (->m void?)]))))
+    (abstract do-command wait-done)))
 
-(define (display? display) (is-a? display display%))
+(define/contract (display? display)
+  (any/c . -> . boolean?)
+  (is-a? display display%))
+
+(module+ test
+  (check-false (display? 1))
+  (let ([display (new (class display%
+                        (super-new)
+                        (define/override (do-command command) (displayln command))
+                        (define/override (wait-done) (sleep 0.01))))])
+    (check-true (display? display))
+    (check-exn exn:fail? (lambda() (send display command)))
+    (check-exn exn:fail? (lambda() (send display command 1)))
+    (check-exn exn:fail? (lambda() (send display ready! 1)))
+    (check-exn exn:fail? (lambda() (send display wait-ready 1)))
+    ))
